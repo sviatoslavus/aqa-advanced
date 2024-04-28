98c04ed9231e3c3da1aca2f614d879852aae1d6f
sviatoslavus <sviatoslav.myhlias@gmail.com>
2024-04-28 14:27:03 +0300

    add primitive operations

---
+++ js-basics/task3.js
@@ -0,0 +1,9 @@
+let firstPersonName = "John"
+let secondPersonName = "Mark"
+
+let greetingConcatenation = "Hello, " + firstPersonName +" and " + secondPersonName + ". Nice to meet you!"
+console.log(greetingConcatenation);
+
+let greetingInterpolation = `Hello, ${firstPersonName} and ${secondPersonName}. Nice to meet you!`
+
+console.log(greetingInterpolation); 
